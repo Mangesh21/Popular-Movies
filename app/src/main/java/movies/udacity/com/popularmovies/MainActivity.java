892662e0db6537657585cc40a3274f2e66a03639
MangeshKadam <mangeshkadam21@gmail.com>
2016-02-22 20:58:02 +0530

    * movie adapter added.

--- app/src/main/java/movies/udacity/com/popularmovies/MainActivity.java
+++ app/src/main/java/movies/udacity/com/popularmovies/MainActivity.java
@@ -4,12 +4,26 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import movies.udacity.com.popularmovies.network.APICallBack;
+import movies.udacity.com.popularmovies.network.APIError;
+import movies.udacity.com.popularmovies.network.BaseClient;
+import movies.udacity.com.popularmovies.network.MovieList;
+import movies.udacity.com.popularmovies.uiutils.MovieDetailAdapter;
+import movies.udacity.com.popularmovies.uiutils.SpacesItemDecoration;
 
 public class MainActivity extends AppCompatActivity {
+
+    BaseClient baseClient = null;
+
+    RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,13 +31,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getMovieList();
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        mRecyclerView = (RecyclerView) findViewById(R.id.moivies_grid);
+        mRecyclerView.setLayoutManager(new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL));
+
+
+    }
+
+    private void getMovieList() {
+        baseClient = new BaseClient();
+        baseClient.init();
+        baseClient.getMoviesList(new APICallBack<MovieList>() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void success(MovieList movieList) {
+                MovieDetailAdapter adapter = new MovieDetailAdapter(MainActivity.this, movieList);
+                mRecyclerView.setAdapter(adapter);
+                SpacesItemDecoration decoration = new SpacesItemDecoration(10, 2);
+                mRecyclerView.addItemDecoration(decoration);
+            }
+
+            @Override
+            public void error(APIError error) {
+                Toast.makeText(MainActivity.this, error.getErrorMessage(), Toast.LENGTH_SHORT).show();
             }
         });
     }
