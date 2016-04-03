c4244794c74590941eeaaf906d9017a6d35ed327
Mangesh <mangesh.kadam@citruspay.com>
2016-04-03 20:18:41 +0530

    * handled only 1 video condition. Added UI for reviews.

--- app/src/main/java/movies/udacity/com/popularmovies/MovieDetailActivity.java
+++ app/src/main/java/movies/udacity/com/popularmovies/MovieDetailActivity.java
@@ -10,6 +10,7 @@ import android.text.SpannableString;
 import android.text.style.RelativeSizeSpan;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -31,11 +32,18 @@ public class MovieDetailActivity extends AppCompatActivity {
     TextView txtRatings;
     TextView txtMovieName;
     TextView txtMovieDetails;
+    TextView review1;
+    TextView review2;
     ImageView imgMovie;
     ImageView imgLike;
 
     ImageView trailer1;
     ImageView trailer2;
+
+    LinearLayout trailerLayout1 = null;
+
+    LinearLayout trailerLayout2 = null;
+
     MovieDetail movieDetail = null;
 
     SQLiteDatabase mSQLiteDatabase = null;
@@ -56,39 +64,6 @@ public class MovieDetailActivity extends AppCompatActivity {
         initReviews();
     }
 
-    private void initReviews() {
-        BaseClient.getInstance().getMovieReviews(String.valueOf(movieDetail.getId()), new APICallBack<MovieReviews>() {
-            @Override
-            public void success(MovieReviews movieReviews) {
-                mMovieReviews = movieReviews;
-                movieDetail.setReview1(mMovieReviews.getResults().get(0).getContent());//Save Review 1
-                movieDetail.setReview2(mMovieReviews.getResults().get(1).getContent());// Save Review 2
-            }
-
-            ;
-
-            @Override
-            public void error(APIError error) {
-                Toast.makeText(MovieDetailActivity.this, error.getErrorMessage(), Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
-
-    private void initTrailers() {
-        BaseClient.getInstance().getMovieTrailers(String.valueOf(movieDetail.getId()), new APICallBack<MovieTrailers>() {
-            @Override
-            public void success(MovieTrailers movieTrailers) {
-                mMovieTrailers = movieTrailers;
-                movieDetail.setMovieTrailerOneID(mMovieTrailers.getResults().get(0).getKey()); //save the trailer One ID
-                movieDetail.setMovieTrailerTwoID(mMovieTrailers.getResults().get(1).getKey());// save the trailer Two ID
-            }
-
-            @Override
-            public void error(APIError error) {
-                Toast.makeText(MovieDetailActivity.this, error.getErrorMessage(), Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
 
     private void initView() {
         txtReleaseDate = (TextView) findViewById(R.id.txtreleasedate);
@@ -99,6 +74,11 @@ public class MovieDetailActivity extends AppCompatActivity {
         imgLike = (ImageView) findViewById(R.id.imglike);
         trailer1 = (ImageView) findViewById(R.id.trailer1);
         trailer2 = (ImageView) findViewById(R.id.trailer2);
+        review1 = (TextView) findViewById(R.id.review1);
+        review2 = (TextView) findViewById(R.id.review2);
+        trailerLayout1 = (LinearLayout) findViewById(R.id.trailersone);
+        trailerLayout2 = (LinearLayout) findViewById(R.id.trailerstwo);
+        trailerLayout2.setVisibility(View.GONE);
         txtReleaseDate.setText(movieDetail.getReleaseDate());
         SpannableString ratingsText = SpannableString.valueOf(movieDetail.getVoteAverage() + "/10");
         ratingsText.setSpan(new RelativeSizeSpan(1.7f), 0, String.valueOf(movieDetail.getVoteAverage()).length(), 0);
@@ -136,6 +116,58 @@ public class MovieDetailActivity extends AppCompatActivity {
 
 
     }
+
+    private void initTrailers() {
+        BaseClient.getInstance().getMovieTrailers(String.valueOf(movieDetail.getId()), new APICallBack<MovieTrailers>() {
+            @Override
+            public void success(MovieTrailers movieTrailers) {
+                mMovieTrailers = movieTrailers;
+                int size = mMovieTrailers.getResults().size();
+                if (size > 0) {
+                    movieDetail.setMovieTrailerOneID(mMovieTrailers.getResults().get(0).getKey()); //save the trailer One ID
+                    if (size > 1) {
+                        trailerLayout2.setVisibility(View.VISIBLE);
+                        movieDetail.setMovieTrailerTwoID(mMovieTrailers.getResults().get(1).getKey());// save the trailer Two ID
+                    }
+                    else {
+                        trailerLayout2.setVisibility(View.GONE); //some movies have only 1 trailer
+                    }
+                } else {
+                    trailerLayout1.setVisibility(View.GONE);
+                    trailerLayout2.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void error(APIError error) {
+                Toast.makeText(MovieDetailActivity.this, error.getErrorMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void initReviews() {
+        BaseClient.getInstance().getMovieReviews(String.valueOf(movieDetail.getId()), new APICallBack<MovieReviews>() {
+            @Override
+            public void success(MovieReviews movieReviews) {
+                mMovieReviews = movieReviews;
+                movieDetail.setReview1(mMovieReviews.getResults().get(0).getContent());//Save Review 1
+                movieDetail.setReview2(mMovieReviews.getResults().get(1).getContent());// Save Review 2
+                review1.setText(movieDetail.getReview1());
+                review2.setText(movieDetail.getReview2());
+            }
+
+            ;
+
+            @Override
+            public void error(APIError error) {
+                Toast.makeText(MovieDetailActivity.this, error.getErrorMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+
+
 
 
     /**
