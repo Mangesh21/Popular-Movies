0616123fcbbf0f71e6453c2a8eb569b926872e52
Mangesh <mangesh.kadam@citruspay.com>
2016-04-04 14:24:11 +0530

    * on options item selcted in fragment.

--- app/src/main/java/movies/udacity/com/popularmovies/GridMoviesFragment.java
+++ app/src/main/java/movies/udacity/com/popularmovies/GridMoviesFragment.java
@@ -1,16 +1,24 @@
 package movies.udacity.com.popularmovies;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import movies.udacity.com.popularmovies.database.MovieDetailContentProvider;
 import movies.udacity.com.popularmovies.network.APICallBack;
 import movies.udacity.com.popularmovies.network.APIError;
 import movies.udacity.com.popularmovies.network.BaseClient;
@@ -28,7 +36,7 @@ import movies.udacity.com.popularmovies.uiutils.SpacesItemDecoration;
  * Use the {@link GridMoviesFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class GridMoviesFragment extends Fragment implements MovieDetailAdapter.OnMovieClickListener{
+public class GridMoviesFragment extends Fragment implements MovieDetailAdapter.OnMovieClickListener {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -46,6 +54,11 @@ public class GridMoviesFragment extends Fragment implements MovieDetailAdapter.O
     MovieDetailAdapter adapter;
 
     private View mView;
+
+    String[] projection = {"id", "title", "poster_path", "vote_average", "release_date", "overview",
+            "review1", "review2", "movieTrailerOneID", "movieTrailerTwoID"
+
+    };
 
     public GridMoviesFragment() {
         // Required empty public constructor
@@ -76,6 +89,7 @@ public class GridMoviesFragment extends Fragment implements MovieDetailAdapter.O
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+        setHasOptionsMenu(true);
     }
 
     @Override
@@ -121,7 +135,6 @@ public class GridMoviesFragment extends Fragment implements MovieDetailAdapter.O
     }
 
 
-
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
@@ -137,6 +150,66 @@ public class GridMoviesFragment extends Fragment implements MovieDetailAdapter.O
     public void onDetach() {
         super.onDetach();
         mListener = null;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        // Do something that differs the Activity's menu here
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_sort_by_favourite:
+
+                Cursor moviesList = getActivity().getContentResolver().query(
+                        MovieDetailContentProvider.CONTENT_URI, projection, null, null, null);
+                List<MovieDetail> results = new ArrayList<MovieDetail>();
+                while (moviesList.moveToNext()) {
+                    results.add(new MovieDetail(
+                            moviesList.getInt(0),
+                            moviesList.getString(1), moviesList.getString(2),
+                            moviesList.getDouble(3), moviesList.getString(4),
+                            moviesList.getString(5), moviesList.getString(6),
+                            moviesList.getString(7), moviesList.getString(8),
+                            moviesList.getString(9), 1
+                    ));
+                }
+                if (adapter == null) {
+                    adapter = new MovieDetailAdapter(getActivity(), GridMoviesFragment.this, results);
+                    mRecyclerView.setAdapter(adapter);
+                } else {
+                    adapter.setMovieDetailsList(results);
+                    adapter.notifyDataSetChanged();
+                }
+
+                Constants.isOrderByRatings = false;
+                Constants.isOrderedByPopularity = false;
+                return true;
+            case R.id.action_sort_by_popularity:
+                if (Constants.isOrderedByPopularity) {
+                    return true;
+                } else {
+                    getMovieList(Constants.ORDER_POPULARITY);
+                }
+
+                Constants.isOrderByRatings = false;
+                Constants.isOrderedByPopularity = true;
+                return true;
+            case R.id.action_sort_by_ratings:
+                if (Constants.isOrderByRatings) {
+                    return true;
+                } else {
+                    getMovieList(Constants.ORDER_RATING);
+                }
+
+                Constants.isOrderByRatings = true;
+                Constants.isOrderedByPopularity = false;
+                return true;
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
--- app/src/main/java/movies/udacity/com/popularmovies/MainActivity.java
+++ app/src/main/java/movies/udacity/com/popularmovies/MainActivity.java
@@ -16,10 +16,7 @@ public class MainActivity extends AppCompatActivity implements GridMoviesFragmen
     // RecyclerView mRecyclerView;
     MovieDetailAdapter adapter;
 
-    String[] projection = {"id", "title", "poster_path", "vote_average", "release_date", "overview",
-            "review1", "review2", "movieTrailerOneID", "movieTrailerTwoID"
 
-    };
 
 
     @Override
@@ -60,62 +57,8 @@ public class MainActivity extends AppCompatActivity implements GridMoviesFragmen
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-     /*   int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_sort_by_popularity) {
-            if (Constants.isOrderedByPopularity) {
-                return true;
-            } else {
-                getMovieList(Constants.ORDER_POPULARITY);
-            }
-
-            Constants.isOrderByRatings = false;
-            Constants.isOrderedByPopularity  = true;
-        }
-
-        if (id == R.id.action_sort_by_ratings) {
-            if (Constants.isOrderByRatings) {
-                return true;
-            } else {
-                getMovieList(Constants.ORDER_RATING);
-            }
-
-            Constants.isOrderByRatings = true;
-            Constants.isOrderedByPopularity  = false;
-        }
-
-        if (id == R.id.action_sort_by_favourite) {
-            Cursor moviesList = MainActivity.this.getContentResolver().query(
-                    MovieDetailContentProvider.CONTENT_URI, projection, null, null, null);
-            List<MovieDetail> results = new ArrayList<MovieDetail>();
-            while (moviesList.moveToNext()) {
-                results.add(new MovieDetail(
-                        moviesList.getInt(0),
-                        moviesList.getString(1), moviesList.getString(2),
-                        moviesList.getDouble(3), moviesList.getString(4),
-                        moviesList.getString(5), moviesList.getString(6),
-                        moviesList.getString(7), moviesList.getString(8),
-                        moviesList.getString(9), 1
-                ));
-            }
-            if (adapter == null) {
-                adapter = new MovieDetailAdapter(MainActivity.this, results);
-                mRecyclerView.setAdapter(adapter);
-            } else {
-                adapter.setMovieDetailsList(results);
-                adapter.notifyDataSetChanged();
-            }
-
-
-            Constants.isOrderByRatings = false;
-            Constants.isOrderedByPopularity  = false;
-        }
-*/
-        return super.onOptionsItemSelected(item);
+        return false;
     }
 
     @Override
